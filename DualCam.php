ef880ee9f13cd03334e0fe16791525bd50167a1f
davidwilliamdonnelly <davidwdonnelly@gmail.com>
2021-04-21 21:01:29 -0400

    added DualCam.php from fishpond pi

---
+++ DualCam.php
@@ -0,0 +1,34 @@
+<?php
+
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title>untitled</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 1.33" />
+</head>
+
+<body>
+	
+<table border="1" style="border-collapse: collapse; width: 100%; height: 1000px">
+<tbody>
+<tr >
+
+<td style="width: 50%">
+   <iframe title="a" src="http://192.168.1.30/CamInterface/PiCam.php"  width="100%" height="100%"></iframe>
+</td>
+<td style="width: 50%">
+   <iframe title="c" src="http://192.168.1.31/CamInterface/PiCam.php" width="100% "height="100%"></iframe>
+</td>
+
+</tr>
+
+</tbody>
+</table>
+</body>
+
+</html>
